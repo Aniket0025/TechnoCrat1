61072a70877d9a056baa958d74e01525edc16c16
Aniket Audumbar Yadav <157941657+Aniket0025@users.noreply.github.com>
2025-04-18 15:46:23 +0530

    Update productService.ts

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,5 +1,5 @@
 export async function fetchEcoProducts() {
-  const response = await fetch("http://localhost:5000/api/products");
+  const response = await fetch("[https://your-backend.onrender.com/api/products");](https://your-backend.onrender.com/api/products");)
   if (!response.ok) throw new Error("Failed to fetch eco products");
   return response.json();
 }
